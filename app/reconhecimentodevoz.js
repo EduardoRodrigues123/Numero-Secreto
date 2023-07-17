475d6f3daaa55332bf49df91f99d1cf0eced21d6
EduardoRodrigues123 <121134383+EduardoRodrigues123@users.noreply.github.com>
2023-07-17 15:16:30 -0300

    Update reconhecimentodevoz.js

--- app/reconhecimentodevoz.js
+++ app/reconhecimentodevoz.js
@@ -4,3 +4,9 @@ const recognition = new SpeechRecognition();
 
 recognition.lang = "pt-Br"
 recognition.start()
+
+recognition.addEventListener('Result', onSpeak)
+
+function onSpeak (e){
+    console.log(e.results[0][0].transcript)
+}
